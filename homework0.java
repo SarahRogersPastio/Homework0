3b90e6ac67be1d1067f613911942f61c1ce2e5bd
SarahRogersPastio <105824671+SarahRogersPastio@users.noreply.github.com>
2024-01-19 14:32:10 -0500

    Updated filename and output
    Changed the name from test to homework0 and added println("Sarah Rogers-Pastio")
    
    (Note to self: This ended up looking like the file was rewritten since test.java is gone now.)

--- homework0.java
+++ homework0.java
@@ -1,6 +1,7 @@
 
-public class test{
+public class homework0{
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Sarah Rogers-Pastio");
     }
 }
