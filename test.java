94e724a833473b708afd243b22eefc897e52ad6a
SarahRogersPastio <105824671+SarahRogersPastio@users.noreply.github.com>
2024-01-19 11:53:28 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 
-public class Test{
+public class test{
     public static void main(String[] args){
-        system.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
